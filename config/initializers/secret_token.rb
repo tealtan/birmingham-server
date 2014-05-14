1525e152f80d2583a8306fc338574263300dc783
Allen Tan <tealtan@Allens-iMac.local>
2014-05-13 22:10:29 -0400

    Set up Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BirminghamServer::Application.config.secret_key_base = 'edbc75542757e0115fe8662ebae2317b1bf7659c838114064b212d1462ab7e5b5e344a0e036262603cbc7052c210f8eaa351324af46de71e78e5cd236a6cf09f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BirminghamServer::Application.config.session_store :cookie_store, key: '_birmingham-server_session'
