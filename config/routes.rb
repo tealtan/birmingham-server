0c09d5529b6e000c5ac545320f01efc14e684a02
Allen Tan <allen@tanmade.com>
2015-03-29 18:01:38 -0400

    Revised routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 BirminghamServer::Application.routes.draw do
-  resources :notebooks
 
+  resources :notebooks
   resources :notes
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'notebooks#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
